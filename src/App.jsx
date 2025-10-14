89d63b2a0b00918ac684fd3335a60aa3313ed85f
AfyanLevon <afyanlevon@gmail.com>
2025-10-14 13:26:22 +0400

    Hero: LEFT image symmetric baseline (bottom-0, translate-y-0) + cache-bust

--- src/App.jsx
+++ src/App.jsx
@@ -19,23 +19,15 @@ function Hero() {
           <div className="hidden md:flex md:col-start-1 md:col-end-2 justify-self-end self-end">
             <div className="w-[260px] sm:w-[300px] lg:w-[360px] aspect-square relative overflow-visible">
               <img
-                src="/img/man-blueprint.png?v=100"
-                alt="Engineer with blueprint"
-                width={360}
-                height={360}
-                decoding="async"
-                loading="eager"
-                draggable={false}
-                className="
-                  absolute bottom-0 left-1/2 -translate-x-1/2
-                  h-full w-auto object-contain origin-bottom
-                  select-none pointer-events-none overflow-visible
-                  transform-gpu will-change-transform
-                  md:translate-x-[0px] lg:translate-x-[0px] xl:translate-x-[0px]
-                  md:translate-y-0 lg:translate-y-0 xl:translate-y-0
-                  md:scale-[1.55] lg:scale-[1.65] xl:scale-[1.70]
-                "
-              />
+  src="/img/man-blueprint.png?v=101"
+  alt="Engineer with blueprint"
+  width={360}
+  height={360}
+  decoding="async"
+  loading="eager"
+  draggable={false}
+  className="absolute bottom-0 left-1/2 -translate-x-1/2 h-full w-auto object-contain origin-bottom md:translate-x-[0px] lg:translate-x-[0px] xl:translate-x-[0px] md:translate-y-0 lg:translate-y-0 xl:translate-y-0 md:scale-[1.55] lg:scale-[1.65] xl:scale-[1.70] select-none pointer-events-none overflow-visible transform-gpu will-change-transform"
+/>
             </div>
           </div>
 
@@ -135,6 +127,7 @@ function AppContent() {
 export default function App() {
   return (<LanguageProvider><AppContent /></LanguageProvider>);
 }
+
 
 
 
